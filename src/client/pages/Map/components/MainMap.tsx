666a327e62f9d9ffc9f020098b54a3868e08a4a6
Kiril Pashkov <k.pashkov@itransition.com>
2022-05-14 18:21:10 +0300

    sdg

--- src/client/pages/Map/components/MainMap.tsx
+++ src/client/pages/Map/components/MainMap.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MapContainer } from 'react-leaflet';
 import { MapTileLayer } from '../../../shared/components/MapTileLayer';
+import { MAIN_MAP_DEFAULTS } from '../../../shared/constants';
 import { LocationMarker } from '../types';
 import { LocationMarkerList } from './LocationMarkerList';
 
@@ -12,7 +13,9 @@ export function MainMap({ markers }: Props) {
   return (
     <MapContainer
       className="main-map"
-      scrollWheelZoom={true}>
+      scrollWheelZoom={true}
+      center={MAIN_MAP_DEFAULTS.CENTER}
+      zoom={MAIN_MAP_DEFAULTS.ZOOM}>
       <MapTileLayer />
       <LocationMarkerList markers={markers} />
     </MapContainer>
--- src/client/shared/constants.ts
+++ src/client/shared/constants.ts
@@ -1,3 +1,5 @@
+import { LatLngLiteral } from "leaflet";
+
 export const USER_ID_TOKEN_KEY = "USER_ID";
 export const USER_DATA_TOKEN_KEY = "USER_DATA";
 export const ACCESS_TOKEN = process.env.MAP_ACCESS_TOKEN;
@@ -8,3 +10,11 @@ export const CHART_COLORS = [
   '#ffcc5c',
   '#ff6f69',
 ];
+
+export const MAIN_MAP_DEFAULTS = {
+  ZOOM: 5,
+  CENTER: {
+    lat: 52.45835188280866,
+    lng: 30.00292968750001
+  } as LatLngLiteral
+};
